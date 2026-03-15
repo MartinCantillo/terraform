7c9a4dc9fd407ef0cbc2ec94e6feab82715efb01
Martin9 <martinjcantillo9@gmail.com>
2026-03-15 08:55:56 -0500

    feat: adding dry principle example

--- practica_2/terraform.tf
+++ practica_2/terraform.tf
@@ -1,9 +1,12 @@
 resource "local_file" "products" {
+  count = 5
   content  = "list of product for the next month"
-  filename = "products -${random_string.sufijo.id}.txt"
+  filename = "products -${random_string.sufijo[count.index].id}.txt"
 }
 
 resource "random_string" "sufijo" {
+
+  count = 5
   length  = 4
   special = false
   upper   = false
